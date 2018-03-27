a4bd76456ec605f26b50edad6902a52da7f722f9
Marek Obuchowicz <marek-obuchowicz@users.noreply.github.com>
2018-03-27 20:24:42 +0200

    Don't enforce empty 70-persistent-net.rules file

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -12,9 +12,8 @@ rm -f /var/log/auth.log /var/log/daemon.log /var/log/messages /var/log/syslog
 # Make sure Udev doesn't block our network
 echo "cleaning up udev rules"
 rm -f /etc/udev/rules.d/70-persistent-net.rules
-mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
-rm /lib/udev/rules.d/75-persistent-net-generator.rules
+rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
 
 echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
